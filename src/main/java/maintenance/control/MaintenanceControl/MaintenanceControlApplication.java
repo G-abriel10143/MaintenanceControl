42618a7ded7ffcc76de171ba1c0eea973314e2c9
Gabriel Cerqueira Alves <101301397+G-abriel10143@users.noreply.github.com>
2023-03-11 11:07:02 -0300

    Add files via upload

---
+++ src/main/java/maintenance/control/MaintenanceControl/MaintenanceControlApplication.java
@@ -0,0 +1,13 @@
+package maintenance.control.MaintenanceControl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaintenanceControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaintenanceControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/maintenance/control/MaintenanceControl/MaintenanceControlApplicationTests.java
@@ -0,0 +1,13 @@
+package maintenance.control.MaintenanceControl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaintenanceControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
