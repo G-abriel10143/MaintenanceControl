e7d649772238fd0ffa893299e23e79ceb1e2bfc4
Gabriel Cerqueira Alves <101301397+Gabriel0030@users.noreply.github.com>
2023-03-11 11:36:17 -0300

    models pronto/ falta apenas arrumar readme

---
+++ src/main/java/maintenance/control/MaintenanceControl/models/Busca.java
@@ -0,0 +1,52 @@
+package maintenance.control.MaintenanceControl.models;
+
+public class Busca {
+
+    int num_login;
+    String id_empresa;
+    String id_marca;
+    String id_modelo;
+
+    public Busca() {
+    }
+
+    public Busca(int num_login, String id_empresa, String id_marca, String id_modelo) {
+        this.num_login = num_login;
+        this.id_empresa = id_empresa;
+        this.id_marca = id_marca;
+        this.id_modelo = id_modelo;
+    }
+
+    public long getnum_login() {
+        return num_login;
+    }
+
+    public void setnum_login(int num_login) {
+        this.num_login = num_login;
+    }
+
+    public String getid_empresa() {
+        return id_empresa;
+    }
+
+    public void setid_empresa(String id_empresa) {
+        this.id_empresa = id_empresa;
+    }
+
+    public String getid_marca() {
+        return id_marca;
+    }
+
+    public void setid_marca(String id_marca) {
+        this.id_marca = id_marca;
+    }
+
+    public String getid_modelo() {
+        return id_modelo;
+    }
+
+    public void setid_modelo(String id_modelo) {
+        this.id_modelo = id_modelo;
+    }
+
+}
