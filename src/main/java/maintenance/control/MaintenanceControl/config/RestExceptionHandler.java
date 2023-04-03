8ed7b9f661e1c4daa90f2deadca296842ae99dae
Gabriel Cerqueira Alves <101301397+Gabriel0030@users.noreply.github.com>
2023-04-03 15:28:15 -0300

    Bean Validation ok

---
+++ src/main/java/maintenance/control/MaintenanceControl/config/RestExceptionHandler.java
@@ -0,0 +1,28 @@
+package br.com.maintenanceControl.config;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import br.com.fiap.maintenanceControl.models.RestValidationError;
+
+@RestControllerAdvice
+public class RestExceptionHandler {
+
+    Logger logger = LoggerFactory.getLogger(RestExceptionHandler.class);
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<RestValidationError>> methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException e){
+        logger.error("Erro de argumento inválido");
+        List<RestValidationError> errorsList = new ArrayList<>();
+        e.getFieldErrors().forEach(err -> errorsList.add(new RestValidationError(400, err.getField(), err.getDefaultMessage())));
+
+
+        return ResponseEntity.badRequest().body(errorsList);
+    }
+}
