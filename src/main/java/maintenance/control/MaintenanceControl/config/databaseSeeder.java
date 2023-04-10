caafbe57c5578c28a26e1d97635c372259792686
Gabriel Cerqueira Alves <101301397+Gabriel0030@users.noreply.github.com>
2023-04-10 19:41:07 -0300

    DataSeeder ok

---
+++ src/main/java/maintenance/control/MaintenanceControl/config/databaseSeeder.java
@@ -0,0 +1,12 @@
+package br.com.maintenanceControl.exceptions;
+
+import java.math.BigDecimal;
+import java.util.Calendar;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DatabaseSeeder implements CommandLineRunner{
